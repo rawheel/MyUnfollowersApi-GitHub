a50fef8f7603b4a3d9ad27fdd44353fc2c7e1ae4
rawheel <rawheeelalisiddiqui@gmail.com>
2020-09-21 19:45:03 +0500

    save_follower is ready

---
+++ check.py
@@ -0,0 +1,53 @@
+import requests
+from pprint import pprint
+import threading
+import math
+import json
+class save_followers:
+    def __init__(self,username):
+        self.username = username
+
+        info=f"https://api.github.com/users/{self.username}"
+        total_followers = requests.get(info).json()
+        self.length = math.ceil((int(total_followers["followers"]))/100)
+        self.followers=[]
+    def call_api(self,page):
+        per_page = 100 
+        url = f"https://api.github.com/users/{self.username}/followers?page={page}&per_page={str(per_page)}"
+        user_data = requests.get(url).json()
+        [self.followers.append(user_data[i]["login"]) for i in range(len(user_data))]
+        
+    def get_followers(self):
+        threads=[]
+        for page in range(1,self.length+1):
+            #get_followers(page)
+            t = threading.Thread(target=self.call_api,args=[page])
+            t.start()
+            threads.append(t)
+        for thread in threads:
+            thread.join()
+        info_dict={self.username:self.followers}
+        '''with open('data.txt', 'w') as outfile:
+            json.dump(info_dict, outfile)'''
+        return info_dict
+
+#object_1 = save_followers("wajahatkarim3")
+
+#saved_followers = object_1.save_followers()
+
+#pprint(json.load(read))
+#print(saved_followers)
+#print(len(saved_followers))
+
+class comp:
+    def __init__(self,username):
+        self.username = username
+
+
+    def read_json(self):
+        read = open("data.txt","r")
+        data = json.load(read)
+        print(data)
+ob  = comp("wajahatkarim3")
+ob.read_json()
+
---
+++ github.py
@@ -0,0 +1,32 @@
+import requests
+from pprint import pprint
+import threading
+import math
+import json
+class save_followers:
+    def __init__(self,username):
+        self.username = username
+
+        info=f"https://api.github.com/users/{self.username}"
+        total_followers = requests.get(info).json()
+        self.length = math.ceil((int(total_followers["followers"]))/100)
+        self.followers=[]
+    def call_api(self,page):
+        per_page = 100 
+        url = f"https://api.github.com/users/{self.username}/followers?page={page}&per_page={str(per_page)}"
+        user_data = requests.get(url).json()
+        [self.followers.append(user_data[i]["login"]) for i in range(len(user_data))]
+        
+    def get_followers(self):
+        threads=[]
+        for page in range(1,self.length+1):
+            #get_followers(page)
+            t = threading.Thread(target=self.call_api,args=[page])
+            t.start()
+            threads.append(t)
+        for thread in threads:
+            thread.join()
+        info_dict={self.username:self.followers}
+        '''with open('data.txt', 'w') as outfile:
+            json.dump(info_dict, outfile)'''
+        return info_dict
