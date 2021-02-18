da5163acd738676e96eb8f300db226df188334ce
rawheel <rawheeelalisiddiqui@gmail.com>
2021-02-18 19:17:57 +0500

    CORS added

--- app.py
+++ app.py
@@ -3,16 +3,18 @@ from flask import Flask,abort
 from flask import jsonify
 from flask import request
 from views import compare_followers
-
+from flask_cors import CORS
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/github/',methods=['GET'])
+
 def github():
     username = str(request.args['username'])
     try:
         result = compare_followers(username)
-        result.headers.add("Access-Control-Allow-Origin", "*")
-        return jsonify(result)
+        response = jsonify(result)
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response 
     except Exception as e:
         print(e)
         abort(400,description="Max Retries exceeded with URL or Invalid Username! take a break man...")
