1b6a7fe7a3df09f4913491e89df108a2d741bd28
rawheel <rawheeelalisiddiqui@gmail.com>
2021-02-19 03:36:49 +0500

    REST API Done!

--- app.py
+++ app.py
@@ -1,40 +1,32 @@
 import threading
 from flask import Flask,abort
 from flask import jsonify
+from flask_cors import CORS
 from flask import request
 from views import compare_followers
 from flask_restful import Resource, Api
 
+
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 
 @app.route('/github/',methods=['GET'])
 
 class Github(Resource):
     def get(self,username):
         try:
+            #abort(400,description="Max Retries exceeded with URL or Invalid Username! take a break man...")
+            #return 400
             result = compare_followers(username)
+            return result
             #response = jsonify(result)
             #response.headers.add("Access-Control-Allow-Origin", "*")
-            return result
+            
         except Exception as e:
-            print(e)
             abort(400,description="Max Retries exceeded with URL or Invalid Username! take a break man...")
+            
 api.add_resource(Github, '/githubapi/<string:username>')
-
-
-'''
-def github():
-    username = str(request.args['username'])
-    try:
-        result = compare_followers(username)
-        response = jsonify(result)
-        response.headers.add("Access-Control-Allow-Origin", "*")
-        return response 
-    except Exception as e:
-        print(e)
-        abort(400,description="Max Retries exceeded with URL or Invalid Username! take a break man...")
-'''
 
 
 if __name__ == "__main__":
